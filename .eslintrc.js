68fdd79e50f949d4bc46216318e770814eef93a2
Anh Dao <addao823@gmail.com>
2019-12-14 09:41:22 -0800

    Reconfigure linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,14 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ["standard", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+    "prettier/react"
+  ],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
@@ -13,7 +20,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module"
   },
-  plugins: ["prettier"],
+  plugins: ["react", "import", "jsx-a11y", "react-hooks", "prettier"],
   rules: {
     "prettier/prettier": ["error"]
   }
